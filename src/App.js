4c1f727346039e82c343348db1012624434784c4
francis omondi <francisomondi17@gmail.com>
2022-06-02 20:17:04 +0300

    dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const title = "Welcome to mawimbi restaurant";
+  const admin = { name: "Francis Aziz", department: "support" };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="content">
+        <h2>{title}</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          my name is {admin.name} and am the {admin.department} person
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }

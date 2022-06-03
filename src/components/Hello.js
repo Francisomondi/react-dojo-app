e98ed0a09510182e27e50240ed0958e888fc2937
francis omondi <francisomondi17@gmail.com>
2022-06-03 20:27:34 +0300

    jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Hello = () => {
+  const displayMessage = () => {
+    return "hello this is the message";
+  };
   return (
     <div>
-      <h2>hello world</h2>
+      <h2>hello, {displayMessage()}</h2>
     </div>
   );
 };

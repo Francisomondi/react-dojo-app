3fb2404c7e6801c2844135d393c592fe40e9d061
francis omondi <francisomondi17@gmail.com>
2022-06-03 20:01:18 +0300

    class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
-  const title = "Welcome to mawimbi restaurant";
-  const admin = { name: "Francis Aziz", department: "support" };
   return (
     <div className="App">
       <div className="content">
-        <h2>{title}</h2>
-        <p>
-          my name is {admin.name} and am the {admin.department} person
-        </p>
         <Hello />
+        <Message />
       </div>
     </div>
   );
---
+++ src/components/Message.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+class Message extends Component {
+  render() {
+    return <p>this is a class component</p>;
+  }
+}
+
+export default Message;

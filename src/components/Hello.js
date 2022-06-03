e2c08158a7c688bfd96f3acd12487b49893a58dc
francis omondi <francisomondi17@gmail.com>
2022-06-04 00:26:48 +0300

    ternary conditional approach

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,15 +3,17 @@ import { useState } from "react";
 
 const Hello = () => {
   const [counter, setCounter] = useState(0);
-  const [display, setDisplay] = useState(true);
+  const [display, setDisplay] = useState(false);
 
-  let output;
-  if (display) {
-    output = <h2>hello, this is a message</h2>;
-  } else {
-    output = <h2>hello, this is not a message</h2>;
-  }
-  return <div>{output}</div>;
+  return display ? (
+    <div>
+      <h2>hello, this is a message</h2>
+    </div>
+  ) : (
+    <div>
+      <h2>hello, this is not a message</h2>
+    </div>
+  );
 };
 
 export default Hello;

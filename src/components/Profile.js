730c349292a5073dfe528817a02fe3f0fa939463
francis omondi <francisomondi17@gmail.com>
2022-06-03 21:39:37 +0300

    handle simple click events

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Profile = props => {
   const { name, lastname } = props;
+
+  const handleButtonClick = () => {
+    console.log("button clicked by ", name);
+  };
   return (
     <div>
       <h1>
         name: {name} {lastname}
       </h1>
+      <button onClick={handleButtonClick}>submit name</button>
     </div>
   );
 };

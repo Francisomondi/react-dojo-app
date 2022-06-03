beafb148032452b100bc416e1ecbaf105fe32051
francis omondi <francisomondi17@gmail.com>
2022-06-03 23:54:58 +0300

    using state in a fuctional component

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 const Hello = () => {
+  const [counter, setCounter] = useState(0);
   const displayMessage = () => {
-    return "hello this is the message";
+    return "this is the message";
   };
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div>
-      <h2>hello, {displayMessage()}</h2>
+      <div>
+        <h2>hello, {displayMessage()}</h2>
+      </div>
+      <div>
+        <h3>Counter value : {counter}</h3>
+      </div>
+      <div>
+        <button onClick={increment}>Increment</button>
+      </div>
     </div>
   );
 };

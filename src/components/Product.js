8e392c0151aaed930b9e1216e31d53853af20c9a
francis omondi <francisomondi17@gmail.com>
2022-06-04 14:51:06 +0300

    rendering list of objects, and asigning a unique id

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,14 +2,34 @@ import React from "react";
 
 const Product = () => {
   const products = [
-    "laptops",
-    "mouse",
-    "telepropters",
-    "desktops",
-    "mobile phones",
-    "ear phones"
+    {
+      id: 1,
+      firstname: "Francis",
+      lastname: "omondi",
+      role: "IT"
+    },
+    {
+      id: 2,
+      firstname: "hamphrey",
+      lastname: "designer",
+      role: "designs"
+    },
+    {
+      id: 3,
+      firstname: "brian",
+      lastname: "otieno",
+      role: "SQ"
+    },
+    {
+      id: 4,
+      firstname: "salome",
+      lastname: "muitaliano",
+      role: "Procuremets"
+    }
   ];
-  const productList = products.map(product => <h2>{product}</h2>);
+  const productList = products.map(product => (
+    <h2 key={product.id}>{product.firstname}</h2>
+  ));
 
   return <div>{productList}</div>;
 };
